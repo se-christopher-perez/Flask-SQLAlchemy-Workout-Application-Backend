f1cab345d20fe125ef4c8961c4952462ae5f0b36
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-20 03:29:03 -0400

    created relationships for Exercise, Workout, and WorkoutExercise

--- server/models.py
+++ server/models.py
@@ -4,40 +4,50 @@ db = SQLAlchemy()
 
 # Define Models here
 
-class Excercise(db.Model):
+class Exercise(db.Model):
 
-    __tablename__ = 'excercise'
+    __tablename__ = 'exercises'
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False, unique = True)
     category = db.Column(db.String, nullable = False)
     equipment_needed = db.Column(db.Boolean, default = False)
 
+    workout_exercises = db.relationship('WorkoutExercise', back_populates = 'exercise', cascade = 'all, delete-orphan')
+    workouts = db.relationship('Workout', secondary = 'workout_exercises', back_populates = 'exercises')
+
+
     def __repr__(self):
         return f"<Exercise id={self.id} name={self.name}>"
 
 class Workout(db.Model):
 
-    __tablename__ = 'workout'
+    __tablename__ = 'workouts'
 
     id = db.Column(db.Integer, primary_key = True)
     date = db.Column(db.Date, nullable = False)
     duration_min = db.Column(db.Integer, nullable = False)
     notes = db.Column(db.Text)
 
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='workout', cascade='all, delete-orphan')
+    exercises = db.relationship('Exercise', secondary='workout_exercises', back_populates='workouts')
+
     def __repr__(self):
         return f"<Workout id={self.id} date={self.date} duration={self.duration_min}>"
 
 class WorkoutExercise(db.Model):
 
-    __tablename__ = 'workout_exercise'
+    __tablename__ = 'workout_exercises'
 
     id = db.Column(db.Integer, primary_key = True)
-    workout_id = db.Column(db.Interger, db.ForeignKey('workouts.id'), nullable = False)
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable = False)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable = False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable = False)
     reps = db.Column(db.Integer)
     sets = db.Column(db.Integer)
     duration_sec = db.Column(db.Integer)
+
+    workout = db.relationship('Workout', back_populates='workout_exercises')
+    exercise = db.relationship('Exercise', back_populates='workout_exercises')
 
     def __repr__(self):
         return f"<WorkoutExercise workout={self.workout_id} excercise={self.exercise_id}>"
